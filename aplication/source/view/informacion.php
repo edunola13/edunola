ac6985f25e7b5af860b9ebd5e60f7e8e274c03ae
edunola13 <edunola13@hotmail.com>
2014-11-30 21:53:19 -0300

    Informacion

--- aplication/source/view/informacion.php
+++ aplication/source/view/informacion.php
@@ -12,7 +12,7 @@
     
         <section class="row">
             <article class="col col-md-2">
-                <?php Tags::image("Eduardo Sebasian Nola", "http://lorempixel.com/140/140/");?>
+                <?php Tags::image("Eduardo Sebasian Nola", BASEURL . 'resources/images/foto-personal.png');?>
             </article>
             <article class="col col-md-6">
                 <?php Tags::paragraph("left"); ?>
